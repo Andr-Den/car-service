f6d115c1d02e0a7e7d33dcc7a1e5dfc4ca64cdc6
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-05-11 00:07:34 +0500

    refactor component Advantages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Advantage from './components/Advantage';
 import Header from './components/Header';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
+import Services from './components/Services';
 
 function App() {
   return (
     <>
       <Header />
       <Advantage />
+      <Services />
       <Footer />
     </>
   );
--- src/components/Advantage.js
+++ src/components/Advantage.js
@@ -3,14 +3,14 @@ import Container from './Container';
 
 function Advantage() {
   return (
-    <div className="bg-tertiary py-[180px] text-primary">
+    <div className="bg-tertiary pt-[180px] text-primary">
       <Container>
         <div className="flex justify-between text-text3 tracking-[0.02em] border-t border-separator_primary">
           <p className="font-bold">ГЛАВНОЕ ПРЕИМУЩЕСТВО КОМПАНИИ</p>
           <p className="text-secondary">#1</p>
         </div>
         <div className="flex justify-between mt-[60px]">
-          <p className="text-headline0 -tracking-[0.04em] text-accent">Работаем <br /> с 2012 года</p>
+          <h1 className="text-headline0 -tracking-[0.04em] text-accent">Работаем <br /> с 2012 года</h1>
           <div className="text-description -tracking-[0.02em] w-[580px]">
             <p>Вашим автомобилем будут заниматься люди с большим опытом, полученным за многие годы и прошедшие множество обучений.</p>
             <p className="mt-8">Сотрудники обладают огромным багажом знаний, применяемых на практике.</p>
