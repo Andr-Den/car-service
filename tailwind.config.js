2fd4c8f058f0ec58d750ea7da5ddb6db4d917750
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-05-10 22:26:25 +0500

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Arial Nova"', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        accent: '#FEC110',
+        separator: '#EEECEC',
+        primary: '#2C2C2C',
+        secondary: '#989898',
+        border: '#313232',
+        separator_primary: '#B6B6B6',
+      },
+      fontSize: {
+        headline3: ['38px', '42px'],
+        headline4: ['21px', '25px'],
+        link: ['15px', '18px'],
+        text1: ['16px', '22px'],
+        text3: ['12px', '24px'],
+      }
+    },
+  },
+  plugins: [],
+}
