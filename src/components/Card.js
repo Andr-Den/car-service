220992d5a1812daf3f989c2dd677083a93dc091f
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-05-11 13:26:43 +0500

    add arrow icon in Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import arrowRight from '../images/arrow-right.svg'
+
 function Card({ src, title, subtitle }) {
   return (
     <div className="w-[650px] text-primary">
       <img src={src} alt="#"/>
-      <div className="bg-white p-10 group hover:bg-accent">
+      <div className="bg-white p-10 group hover:bg-accent flex justify-between items-center">
         <div className="text-headline4 -tracking-[0.01em]">
           <h3>{title}</h3>
           <p className="text-secondary group-hover:text-white">{subtitle}</p>
+        </div>
+        <div className="bg-accent rounded-full w-[40px] h-[40px] flex justify-center items-center group-hover:bg-primary">
+          <img src={arrowRight} alt="" className="group-hover:fill-white"/>
         </div>
       </div>
     </div>
