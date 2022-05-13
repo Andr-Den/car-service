92252beebc1bc7c8c25a1244e8ab20a84d80ee90
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-05-13 16:03:12 +0500

    add adaptive for Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -44,15 +44,17 @@ function Hero() {
       <Container>
         <div className="relative flex space-x-10">
           <div className="pt-20">
-            <h2 className="text-headline1">Специализированный <br />Aвтосервис</h2>
-            <h2 className="mt-10 text-headline1 text-accent">+7 (999) 123-45-67</h2>
-            {items.map(({ title, subtitle }) => (
-              <div key={title}>
-                <p>{title}</p>
-                <p>{subtitle}</p>
-              </div>
-            ))}
-            <button className="px-5 py-3 rounded-md bg-accent text-primary mt-[60px]"><a href="#services">Смотреть услуги &darr;</a></button>
+            <h2 className="text-headline1 -tracking-[0.02em] lg:text-tablet1 xs:text-mobile1">Специализированный <br />Aвтосервис</h2>
+            <h2 className="mt-10 text-headline1 text-accent -tracking-[0.02em] lg:text-tablet1  xs:text-mobile1">+7 (999) 123-45-67</h2>
+            <div  className="space-y-2.5 mt-[60px] lg:mt-10 lg:space-y-3.5">
+              {items.map(({ title, subtitle }) => (
+                <div key={title}>
+                  <p className="text-text1 text-secondary xs:text-sm">{title}</p>
+                  <p className="text-text1 xs:text-sm">{subtitle}</p>
+                </div>
+              ))}
+            </div>
+            <button className="px-5 py-3 rounded-md bg-accent text-primary mt-[60px] xs:mt-10"><a href="#services">Смотреть услуги &darr;</a></button>
           </div>
         </div>
       </Container>
@@ -69,12 +71,12 @@ function Hero() {
             {images.map(({ src, index }) => (
               <SwiperSlide key={index}>
                 <img src={src} alt="" className="relative"/>
-                <p className="absolute bottom-[68px] right-[130px] text-headline3">{index}/3</p>
-                <div className="flex space-x-[5px] absolute bottom-[60px] left-10">
-                  <div className="hero-prev-button bg-accent rounded-full w-[60px] h-[60px] flex justify-center items-center hover:cursor-pointer hover:bg-white">
+                <p className="absolute bottom-[68px] right-[130px] text-headline3 sm:bottom-[242px] sm:right-[15px] sm:text-mobile3">{index}/3</p>
+                <div className="flex space-x-[5px] absolute bottom-[60px] left-10 sm:bottom-[233px] sm:left-[15px]">
+                  <div className="hero-prev-button bg-accent rounded-full w-[60px] h-[60px] flex justify-center items-center hover:cursor-pointer hover:bg-white sm:w-[50px] sm:h-[50px]">
                     <ArrowLeft className="stroke-primary"/>
                   </div>
-                  <div className="hero-next-button bg-accent rounded-full w-[60px] h-[60px] flex justify-center items-center hover:cursor-pointer  hover:bg-white">
+                  <div className="hero-next-button bg-accent rounded-full w-[60px] h-[60px] flex justify-center items-center hover:cursor-pointer  hover:bg-white sm:w-[50px] sm:h-[50px]">
                     <ArrowRight className="stroke-primary"/>
                   </div>
                 </div>
