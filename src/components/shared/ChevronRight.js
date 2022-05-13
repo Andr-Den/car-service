8f60509ee15aa8878b259956dc8c250bb1b174f6
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-05-13 14:05:52 +0500

    create new component for chevron

---
+++ src/components/shared/ChevronRight.js
@@ -0,0 +1,7 @@
+export default function ChevronRight({className}) {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
+      <path d="M10 6L16 12L10 18" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+)
+}
