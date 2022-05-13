42bffd5d2d50506113e48330633c0632687bb1e1
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-05-13 20:44:05 +0500

    add adaptive for Lists

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -21,8 +21,8 @@ const CentersItems = [
 function Lists() {
   return (
     <div className="flex mt-10 space-x-10 xs:flex-col xs:space-x-0">
-      <div className="w-[420px]">
-        <p className="text-headline4 text-secondary pb-5 border-b -tracking-[0.01em]">Автоцентры</p>
+      <div className="w-[420px]  xs:w-[345px]">
+        <p className="text-headline4 text-secondary pb-5 border-b -tracking-[0.01em] xs:w-[345px]">Автоцентры</p>
         <ul className="relative flex flex-col space-y-2.5 my-5 text-link">
             {CentersItems.map((name) => (
               <li key={name}>
@@ -31,8 +31,8 @@ function Lists() {
             ))}
         </ul>
       </div>
-      <div className="w-[420px]">
-        <p className="text-headline4 text-secondary pb-5 border-b -tracking-[0.01em]">Услуги</p>
+      <div className="w-[420px]  xs:w-[345px]">
+        <p className="text-headline4 text-secondary pb-5 border-b -tracking-[0.01em] xs:w-[345px]">Услуги</p>
         <ul className="relative flex flex-col space-y-2.5 my-5 text-link">
             {services.map((name) => (
               <li key={name}>
