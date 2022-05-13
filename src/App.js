3ab412994e57c2ec424cd65b1eb8c4ebd1cceacd
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-05-13 08:45:02 +0500

    add component Advice

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Advantage from './components/Advantage';
+import Advice from './components/Advice';
 import Comments from './components/Comments';
 import Companies from './components/Companies';
 import Header from './components/Header';
@@ -9,6 +10,8 @@ import MobApp from './components/MobApp';
 import Services from './components/Services';
 import Stocks from './components/Stocks';
 import Theses from './components/Theses';
+
+import './App.css'
 
 function App() {
   return (
@@ -22,6 +25,7 @@ function App() {
       <Stocks />
       <Comments />
       <Map />
+      <Advice />
       <MobApp />
       <Footer />
     </>
---
+++ src/components/Advice.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Container from "./Container";
+
+import chevronRight from '../images/chevron-right.svg'
+
+import './Advice.css'
+
+function Advice() {
+  return (
+    <div className="text-white wrapper">
+      <div className="pt-[180px]">
+        <Container>
+          <p className="text-text3  border-white border-t pt-2.5">ПОЛУЧИТЬ КОНСУЛЬТАЦИЮ</p>
+          <div className=" w-[650px] float-right mt-[60px]">
+            <p className="text-description">
+              Оставьте заявку, и наш мастер перезвонит вам,
+              произведет предварительную оценку стоимости услуг и запчастей, 
+              и подберет удобное время для визита.
+            </p>
+            <button className="flex px-5 py-3 mt-10 rounded-md bg-accent text-primary hover:cursor-pointer">Оставить заявку <img src={chevronRight} alt="" /></button>
+          </div>
+        </Container>
+      </div>
+    </div>
+  )
+}
+
+export default Advice
